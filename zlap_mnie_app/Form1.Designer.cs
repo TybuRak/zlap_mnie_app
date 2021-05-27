c356450ec556cf1d696c87b30140dc0bbd665584
TybuRak <biuro@tyburskistudio.pl>
2021-05-28 00:09:17 +0200

    Chce zdać, prosze kupon do następnej klasy.

---
+++ zlap_mnie_app/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace zlap_mnie_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timer1 = new System.Timers.Timer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize) (this.timer1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Złap mnie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.toolStripLabel1, this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 371);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(798, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(47, 22);
+            this.toolStripLabel1.Text = "Punkty:";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(64, 22);
+            this.toolStripLabel2.Text = "Highscore:";
+            this.toolStripLabel2.Click += new System.EventHandler(this.toolStripLabel2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 396);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.timer1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Timers.Timer timer1;
+
+        #endregion
+    }
+}
---
+++ zlap_mnie_app/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace zlap_mnie_app
+{
+    public partial class Form1 : Form
+    {
+        readonly Random r;
+        private int Punkty;
+        private int Highscore;
+
+        public Form1()
+        {
+            InitializeComponent();
+            r = new Random();
+            Punkty = 0;
+            Highscore = 0;
+            timer1.Start();
+           
+            
+        }
+
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            MoveButton();
+            Punkty = 0;
+            toolStripLabel1.Text = "Punkty: " + Punkty.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MoveButton();
+            Punkty++;
+            toolStripLabel1.Text = "Punkty: " + Punkty.ToString();
+            timer1.Stop();
+            timer1.Start();
+            button1.BackColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), 0);
+            toolStripLabel2.Text = "Highscore: " + Punkty.ToString();
+            timer1.Stop();
+            timer1.Interval -= 50;
+            timer1.Start();
+        }
+
+        private void MoveButton()
+        {
+            int maxX = this.Size.Width - button1.Size.Width - 50;
+            int maxY = this.Size.Height - button1.Size.Height - 50;
+            Point p = new Point();
+            p.X = r.Next(12, maxX);
+            p.Y = r.Next(12, maxY);
+            button1.Location = p;
+            button1.BackColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), 0);
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
